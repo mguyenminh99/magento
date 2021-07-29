a76f4318918ffd869dd2c5641b37edd83a722a4a
minhnv2 <mguyenminh99@gmail.com>
2021-07-29 22:01:33 +0700

    add note to checkout

--- app/code/Minh/CustomNote/Observer/Checkout/AddNoteToCheckout.php
+++ app/code/Minh/CustomNote/Observer/Checkout/AddNoteToCheckout.php
@@ -1,18 +1,34 @@
 <?php
 namespace Minh\CustomNote\Observer\Checkout;
 
+
 class AddNoteToCheckout implements \Magento\Framework\Event\ObserverInterface
 {
-    public function __construct()
+    protected $request;
+    
+    public function __construct(\Magento\Framework\App\RequestInterface $request)
     {
+        $this->request = $request;
     }
+    /**
+     * Assign quote to session
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
+        // $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/minh.log');
+        //     $logger = new \Zend\Log\Logger();
+        //     $logger->addWriter($writer);
+        // $attributes = implode( " " , $order->getData('customer_note') );
+        // $logger->info($order->getData('customer_note'));
+
         $requestContent = json_decode($this->request->getContent(), true);
         $order = $observer->getEvent()->getOrder();
-        $order->setCustomerNoteNotify(0);
 
+        $order->setCustomerNoteNotify(0);
         if($customerNote = $requestContent['paymentMethod']['extension_attributes']['customer_note'] ?? null){
             $order->setData(
                 'customer_note',
