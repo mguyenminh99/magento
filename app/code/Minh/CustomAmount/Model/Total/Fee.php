a901a58803fee30c8d9a9faadaf8d9dbe5da510f
minhnv2 <mguyenminh99@gmail.com>
2021-08-04 10:56:35 +0700

    import

---
+++ app/code/Minh/CustomAmount/Model/Total/Fee.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Minh\CustomAmount\Model\Total;
+
+
+class Fee extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
+{
+    /**
+     * Collect grand total address amount
+     *
+     * @param \Magento\Quote\Model\Quote $quote
+     * @param \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment
+     * @param \Magento\Quote\Model\Quote\Address\Total $total
+     * @return $this
+     */
+    protected $quoteValidator = null;
+
+    public function __construct(\Magento\Quote\Model\QuoteValidator $quoteValidator)
+    {
+        $this->quoteValidator = $quoteValidator;
+    }
+    public function collect(
+        \Magento\Quote\Model\Quote $quote,
+        \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment,
+        \Magento\Quote\Model\Quote\Address\Total $total
+    ) {
+        parent::collect($quote, $shippingAssignment, $total);
+        $price = 100;
+        $total->setGrandTotal($total->getGrandTotal() + $price);
+
+        return $this;
+    }
+
+    /**
+     * @param \Magento\Quote\Model\Quote $quote
+     * @param Address\Total $total
+     * @return array|null
+     */
+    /**
+     * Assign subtotal amount and label to address object
+     *
+     * @param \Magento\Quote\Model\Quote $quote
+     * @param Address\Total $total
+     * @return array
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function fetch(\Magento\Quote\Model\Quote $quote, \Magento\Quote\Model\Quote\Address\Total $total)
+    {
+        return [
+            'code' => 'fee',
+            'title' => 'Vnext Custom Amount',
+            'value' => 100
+        ];
+    }
+
+    /**
+     * Get Subtotal label
+     *
+     * @return \Magento\Framework\Phrase
+     */
+    public function getLabel()
+    {
+        return __('Vnext Custom Amount');
+    }
+}
