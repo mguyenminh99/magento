98c24b260854322ee0e3b572d21485737246269e
minhnv2 <mguyenminh99@gmail.com>
2021-08-05 09:15:13 +0700

    custom import

--- app/code/Minh/CustomImport/Model/Import/CustomImport/RowValidatorInterface.php
+++ app/code/Minh/CustomImport/Model/Import/CustomImport/RowValidatorInterface.php
@@ -1,58 +1,15 @@
 <?php
-namespace Minh\CustomImport\Model\Import\CustomImport;
+namespace MInh\CustomImport\Model\Import\CustomImport;
 
-class RowValidatorInterface extends \Magento\Framework\Model\AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
+interface RowValidatorInterface extends \Magento\Framework\Validator\ValidatorInterface
 {
-    const CACHE_TAG = 'minh_customimport_row_validator_interface';
-
+    const ERROR_INVALID_TITLE= 'InvalidValueTITLE';
+    const ERROR_MESSAGE_IS_EMPTY = 'EmptyMessage';
+    const ERROR_TITLE_IS_EMPTY = 'EmptyMessageTitle';
     /**
-     * Model cache tag for clear cache in after save and after delete
+     * Initialize validator
      *
-     * @var string
+     * @return $this
      */
-    protected $_cacheTag = self::CACHE_TAG;
-
-    /**
-     * Prefix of model events names
-     *
-     * @var string
-     */
-    protected $_eventPrefix = 'row_validator_interface';
-
-    /**
-     * @param \Magento\Framework\Model\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
-    }
-
-    /**
-     * Initialize resource model
-     *
-     * @return void
-     */
-    protected function _construct()
-    {
-        $this->_init('Minh\CustomImport\Model\ResourceModel\RowValidatorInterface');
-    }
-
-    /**
-     * Return a unique id for the model.
-     *
-     * @return array
-     */
-    public function getIdentities()
-    {
-        return [self::CACHE_TAG . '_' . $this->getId()];
-    }
+    public function init($context);
 }
